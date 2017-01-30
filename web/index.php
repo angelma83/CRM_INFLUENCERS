1e128700c2f995eee33ac4271eb85f74becd92fc
Macanudo chico <cmartinezdelgado@gmail.com>
2017-01-30 20:05:55 +0100

    Subo archivos usados para la web lunes 30/1/2017

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRM Influencers</title>
+	<link rel="stylesheet" type="text/css" href="css/crm_influencers.css">
+</head>
+<body>
+<div id="container">
+<header>
+	<div id="logo">
+	<img  src="img/logo.gif">
+	</div>
+</header>
+	
+</div>
+
+</body>
+</html>
