b1e6fd9bcaff9072c07e625904cf2558fe12b292
Lorena Gómez Pérez <lorena_lgp@MacBook-Pro-de-Lorena.local>
2017-01-30 11:57:58 +0100

    Subida api Twitter

---
+++ formulario_prueba.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<form class="formulario" action="index.php" method="post" enctype="multipart/form-data">
+				<label>Nombre de lista</label>
+				<input type="text" class="cajas" name="lista" title="Nombre" required placeholder="Escribe tu nombre" size="35"><br>
+				<label>Keyword</label>
+				<input type="text" class="cajas" name="keyword" title="Apellidos" required placeholder="Escribe tus apellidos" size="35"><br>
+				<label>Redes sociales</label>
+				<input type="email" class="cajas" name="rrss" title="Email" required placeholder="Indica un email de contacto" size="35"><br>
+				<label>Seguidores</label>
+				<input type="num" class="cajas" name="seguidores" title="telefono" required placeholder="Indica un teléfono" size="35"><br><br>
+				<label>Localización</label><br>
+				<input type="text" name="localizacion" value="Músico">
+				<button name"submit" type="submit" id="contacto" data-submit="sending">Enviar</button>
+			</form>	
+		</div>
+		</html>
