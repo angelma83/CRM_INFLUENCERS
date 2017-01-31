f9d4d739a2d92eb68ca27d0097706d4458e4d389
Macanudo chico <cmartinezdelgado@gmail.com>
2017-01-31 13:47:54 +0100

    Animación y css mejorado

--- web2/index.php
+++ web2/index.php
@@ -4,6 +4,8 @@
 	<title>CRM Influencers</title>
 	<link rel="stylesheet" type="text/css" href="css/crm_influencers.css">
 	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 </head>
 <body>
 
@@ -32,13 +34,25 @@
 
 		<div class="wrapper-menu">
 
-                <h1>¿Qué quieres hacer?</h1>
-                <a href="formulario.php" >Crear lista</a>
+                <h1 class="animacion1">¿Qué quieres hacer?</h1>
+                <a class="animacion2" href="formulario.php" >Crear lista</a>
                 <br><br>
-                <a href="">Mostrar todas las listas</a>
-              
+                <a class="animacion3" href="listas.php">Mostrar todas las listas</a>
          </div>
+         <script type="text/javascript">
+		$(document).ready(function(){
+			$(".animacion1").animate({top:'0px', opacity:'1'}, "slow");
+		
+		$(".animacion2").delay(600).animate({opacity:'1'}, "slow");
+		$(".animacion3").delay(800).animate({opacity:'1'}, "slow");
+		});
+		
 
+
+         </script>
+
+<div class="header-1" >También puedes buscar <br>una lista ya creada<form action="listas.php" method="post"> <br><input placeholder="Buscar..." type="search" name="buscar"><br><button type="submit" name="buscar">ENVIAR</button></form>
+</div>
 		
 
 
