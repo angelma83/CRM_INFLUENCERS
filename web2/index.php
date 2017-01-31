e7385b1eede90c97de1d27b6aa5975c647f44421
Laura Adrover <lauradrover@gmail.com>
2017-01-31 12:04:09 +0100

    Añadido versión web 02 - 31/01/17

---
+++ web2/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRM Influencers</title>
+	<link rel="stylesheet" type="text/css" href="css/crm_influencers.css">
+	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+</head>
+<body>
+
+<!-- CONTENEDOR GENERAL -->
+<div id="container">
+
+
+<!-- CONTENEDOR GENERAL -->
+
+		<header>
+			<div id="logo">
+			<img align="center"  src="img/logo.gif" width="350px">
+			</div>
+		</header>
+
+
+<!-- MENÚ INICIAL -->
+
+
+<div id="contenido" align="center">
+	
+	
+
+			<div id="amarillo">	</div>
+			<div id="fondo" style="background-image: url('img/fondo.jpg');opacity: 0.1;" ></div>
+
+		<div class="wrapper-menu">
+
+                <h1>¿Qué quieres hacer?</h1>
+                <a href="formulario.php" >Crear lista</a>
+                <br><br>
+                <a href="">Mostrar todas las listas</a>
+              
+         </div>
+
+		
+
+
+</div>
+
+
+</div>
+
+</body>
+</html>
