f3797f925899a23de4f82a52b97654eac84f7cef
Macanudo chico <cmartinezdelgado@gmail.com>
2017-02-06 10:37:27 +0100

    Mi primer push en terminal

--- web8/RegistroUsuarios.php
+++ web8/RegistroUsuarios.php
@@ -19,24 +19,21 @@ echo "<table width='80%'>";
     $idStr = addslashes($tweet['id_str']);
     $enlacePublicacion = addslashes("https://twitter.com/$idTwitter/status/$idStr");
 
-    
-    /*echo "<tr class='tr100'><td><b>Nombre usuario<b></td> <td>$nombreUsuario</td></tr>";
-    echo "<tr class='tr90'><td><b>ID Twitter<b></td> <td><a href='$enlacePerfil'>$idTwitter</a></td></tr>";
-    echo "<tr class='tr80'><td><b>Descripcion<b></td> <td>$descripcion </td></tr>";
-    echo "<tr class='tr70'><td><b>Seguidores<b></td> <td>$followers</td></tr>";
-    echo "<tr class='tr60'><td><b>Localidad<b></td> <td>$localizacion</td></tr>";
-    echo "<tr class='tr50'><td><b>Texto<b></td> <td>$text</td></tr>";*/
-   // echo "Enlace perfil: $enlacePerfil";
-    //echo "Fecha creación: $fechaCreacion <br />";
 
-     echo "<tr> <td class='tr100'>$nombreUsuario</td>
+  echo "<tr>
     <td class='tr90'  width='170px'><a class='perfil' target='_blank' href='$enlacePerfil'>$idTwitter</a></td>
     <td class='tr80' width='170px'>$descripcion </td>
     <td class='tr70' width='170px'>$followers</td>
     <td class='tr60' width='170px'>$localizacion</td>
     <td class='tr50' width='170px'><a class='perfil' target='_blank' href='$enlacePublicacion'>$text</a></td>
+    <td class='tr100'>$nombreUsuario</td>
+    <td class='tr60' width='170px'><ul>
+    <li>< class='twitter-follow-button' data-show-count='false'>Follow</a> </li>
+
+  
+  <li>mensaje directo</li></td>
+
     </tr>";
-    
 
 
 
@@ -52,7 +49,6 @@ echo "<table width='80%'>";
 
     }
   }
-  echo "<br><br/>";
     echo "</table>";
   // falta poner un if. 
   // header('Location:nohayresultados.php');
